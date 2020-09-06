a2868becdbf1f72a35c0239ad01cf8c7dae18198
Hudson <hudsonmpace@gmail.com>
2020-09-06 11:25:13 -0700

    Load all required assets at the start.

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,51 @@
 import ViewManager from './view-manager.js';
 
-function preloadImage(name, src, imageLoadedCallback) {
-  const image = new Image();
-  image.src = src;
-  image.onload = () => imageLoadedCallback(name, image);
+function loadAsset(src, assetLoadedCallback) {
+  const [,, extension] = src.split('.');
+  let asset;
+  switch (extension) {
+    default:
+      console.log(`'.${extension}' files are not supported. Could not load '${src}'.`);
+      break;
+    case 'png':
+      asset = new Image();
+      asset.src = src;
+      asset.onload = () => assetLoadedCallback(asset);
+      break;
+    case 'json':
+      $.getJSON(src, (data) => {
+        assetLoadedCallback(data);
+      });
+      break;
+  }
 }
-const images = {};
-function preloadImages(allImagesLoadedCallback) {
-  const sourcePath = './assets/images';
-  const imageNames = [
-    'nodeCAR', 'nodeDD', 'nodeLMM', 'nodePEDC', 'nodePharmhaus', 'nodeSmart', 'nodeUnknown', 'nodeWarez', 'agents', 'tileOverlays', 'agentDone', 'items',
+
+const assets = {};
+assets.images = {};
+function loadAssets(allAssetsLoadedCallback) {
+  const sourcePath = './assets';
+  const assetNames = [
+    'images/nodeCAR.png', 'images/nodeDD.png', 'images/nodeLMM.png', 'images/nodePEDC.png', 'images/nodePharmhaus.png', 'images/nodeSmart.png',
+    'images/nodeUnknown.png', 'images/nodeWarez.png', 'images/agents.png', 'images/tileOverlays.png', 'images/agentDone.png', 'images/items.png',
+    'worlds/nightfall_incident/battles.json', 'worlds/nightfall_incident/netmap.json', 'agents.json', 'commands.json',
   ];
   let loadedCount = 0;
 
-  function imageLoadedCallback(name, image) {
-    loadedCount += 1;
-    images[name] = image;
-    console.log(`Image '${name}' loaded.`);
-    if (loadedCount === imageNames.length) {
-      allImagesLoadedCallback(images);
-    }
-  }
-  imageNames.forEach((imageName) => {
-    preloadImage(imageName, `${sourcePath}/${imageName}.png`, imageLoadedCallback);
+  assetNames.forEach((source) => {
+    const words = source.split('/');
+    const name = words[words.length - 1].slice(0, words[words.length - 1].indexOf('.'));
+    loadAsset(`${sourcePath}/${source}`, (asset) => {
+      loadedCount += 1;
+      if (source.startsWith('images/')) {
+        assets.images[name] = asset;
+      } else {
+        assets[name] = asset;
+      }
+      console.log(`'${source}' loaded.`);
+      if (loadedCount === assetNames.length) {
+        allAssetsLoadedCallback(assets);
+      }
+    });
   });
 }
 
@@ -30,8 +53,8 @@ $(document).ready(() => {
   const canvas = $('canvas')[0];
   canvas.height = 500;
   canvas.width = 1000;
-  preloadImages(() => {
-    console.log('All images loaded.');
-    ViewManager('./assets/worlds/nightfall_incident', images);
+  loadAssets(() => {
+    console.log('All assets loaded.');
+    ViewManager('./assets/worlds/nightfall_incident', assets);
   });
 });
