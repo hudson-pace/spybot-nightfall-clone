0f1804f71ba5d560a5cb1c65f121500ab4fff88b
Hudson <hudsonmpace@gmail.com>
2021-01-18 13:43:38 -0700

    Initialize spring boot api

---
+++ api/src/main/java/com/hudsonotron/spybot_api/SpybotApiApplication.java
@@ -0,0 +1,13 @@
+package com.hudsonotron.spybot_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpybotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpybotApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/hudsonotron/spybot_api/SpybotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudsonotron.spybot_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpybotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
