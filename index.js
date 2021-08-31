c4bac095558c1f1c662af39f416840fcb2e5cdbc
Hudson Pace <hudsonmpace@gmail.com>
2021-08-31 01:40:19 -0400

    change to whole node app.

--- index.js
+++ index.js
@@ -6,23 +6,18 @@ const apiRoutes = require('./api/routes')
 const app = express();
 app.use(express.static('public'))
 app.use('/api', apiRoutes);
-console.log('ffj')
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((hey) => {
-    console.log('yo!')
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('db connection established.\n')
   })
   .catch((err) => {
-    console.log('shiz')
-  })
-  /*
-const db = mongoose.connection;
-db.on('error', () => {
-  console.log('oh shiz')
-});
-db.once('open', () => {
-  console.log('connected to db.');
-  app.listen(port, () => {
-    console.log(`listening on port ${port}...`);
+    console.error(`db error: ${err.message}\n`)
   });
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}...\n`);
 });
-*/
